a5df8662cd2d36dd750b037a7cb7f97cb403654f
Amitesh Mani Tiwari <amiteshmanitiwari1997@gmail.com>
2021-05-06 19:16:39 +0530

    Difference queryselectorAll or queryselector

--- Modal/starter/script.js
+++ Modal/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+
+const overlay  = document.querySelector('.overlay');
+
+const closemodal = document.querySelector('.close-modal');
+//queryselector select only one query
+
+// we need to select all query
+const openmodal = document.querySelectorAll('.show-modal');
+
+for(let i=0;i<openmodal.length;i++){
+    console.log(openmodal[i].textContent);
+}
